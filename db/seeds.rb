89970aad0ef73135515f11aede0f703f53f92743
arrubo <turanzas.ror@gmail.com>
2017-07-03 16:19:35 +0200

    Devise added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.new
+u.name = "Paco"
+u.email = "paco@gmail.com"
+u.password = 123456
+u.skip_confirmation!
+u.save
